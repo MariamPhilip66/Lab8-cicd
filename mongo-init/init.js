9865e31c6b55aed45956964e9e4181b92495c1bc
MariamPhilip66 <lolenasinrafah.com>
2026-04-11 05:19:42 +0000

    Add Tea to task list

--- mongo-init/init.js
+++ mongo-init/init.js
@@ -3,6 +3,7 @@ db.tasks.drop();
 db.tasks.insertMany([
   { title: 'Buy groceries', completed: false },
   { title: 'Read a book',   completed: false },
-  { title: 'Go for a run',  completed: true  }
+  { title: 'Go for a run',  completed: true  },
+  { title: 'Tea',           completed: false }
 ]);
 print('Database seeded successfully');
